4a217a8cab70b43bd0634169f49d330314a139a7
Savon <savonstephens4@gmail.com>
2023-08-12 18:52:27 -0700

    petOwners model

--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -5,5 +5,19 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace pet_hotel
 {
-    public class PetOwner {}
+    public class PetOwner 
+    {  
+        //owners Id
+        public int Id { get; set; }
+
+        //owners name
+        public string Name { get; set; }
+
+        //owners email 
+        public string Email { get; set; }
+
+        //the number of pets they own 
+        public int PetCount { get; set; }
+
+    }
 }
