9c1c6860587e0696d2c5a12810441df0d695369b
Peder Goodman <peder.goodman@gmail.com>
2023-08-17 11:29:03 -0500

    fixed error shit

--- Controllers/PetsController.cs
+++ Controllers/PetsController.cs
@@ -59,7 +59,7 @@ namespace pet_hotel.Controllers
             _context.Add(pet);
             _context.SaveChanges();
             
-            return CreatedAtAction(nameof(CreatePet), new {id = pet.ID}, pet);
+            return CreatedAtAction(nameof(CreatePet), new {id = pet.Id}, pet);
         }
 
 
