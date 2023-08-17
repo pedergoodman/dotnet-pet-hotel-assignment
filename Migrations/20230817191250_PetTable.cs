afd608035569fd68b70cbf3d8738874003e6c5a4
Peder Goodman <peder.goodman@gmail.com>
2023-08-17 14:18:29 -0500

    update database

---
+++ Migrations/20230817191250_PetTable.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pet_hotel.Migrations
+{
+    public partial class PetTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pets",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Breed",
+                table: "Pets",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Pets",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pets_OwnerId",
+                table: "Pets",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Pets_PetOwners_OwnerId",
+                table: "Pets",
+                column: "OwnerId",
+                principalTable: "PetOwners",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Pets_PetOwners_OwnerId",
+                table: "Pets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pets_OwnerId",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "Breed",
+                table: "Pets");
+
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Pets");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Pets",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
