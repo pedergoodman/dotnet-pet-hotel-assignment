3c073880b512325012c8d7c8e3154ab059348c82
Justin Lawson <jstn.lawson@gmail.com>
2023-08-17 10:15:26 -0500

    database built

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -2,10 +2,33 @@ using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System;
+using Microsoft.AspNetCore.SignalR;
 
 namespace pet_hotel
 {
-    public enum PetBreedType {}
-    public enum PetColorType {}
-    public class Pet {}
+    public enum PetBreedType {
+        Shepherd,
+        Poodle,
+        Bulldog,
+        Terrier,
+        Boxer,
+        Labrador,
+        Retriever,
+    }
+    public enum PetColorType {
+        Black,
+        White,
+        Golden,
+        Tricolor,
+        Spotted,
+    }
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+         public bool CheckIn { get; set; }
+         [ForeignKey("ownedBy")]
+         public int OwnerId { get; set; }
+
+    }
 }
--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -5,5 +5,10 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace pet_hotel
 {
-    public class PetOwner {}
+    public class PetOwner {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Pets { get; set; }
+    }
 }
